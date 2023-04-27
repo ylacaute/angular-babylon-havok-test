5b6e0aea5627e33c50b570c94bee2ffbce19d5cf
ylacaute <yannick-externe.lacaute@datanumia.com>
2023-04-27 17:38:00 +0200

    feat: babylon show case with wasm error when using havok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IPhysicsEngine} from "@babylonjs/core/Physics/IPhysicsEngine";
+import HavokPhysics from "@babylonjs/havok";
+import {Engine, HavokPlugin, Scene, Vector3} from "@babylonjs/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-havok';
+
+  ngOnInit(): void {
+    const canvas = document
+      .getElementById('gameCanvas') as HTMLCanvasElement;
+    const engine = new Engine(canvas, true);
+    const scene = new Scene(engine);
+    HavokPhysics().then(havok => {
+      let havokPlugin = new HavokPlugin(false, havok);
+      scene.enablePhysics(new Vector3(0, -9.81, 0), havokPlugin);
+      this.init(engine, scene);
+    })
+  }
+
+  init(engine: Engine, scene: Scene): void {
+    let physEngine: IPhysicsEngine | null = scene.getPhysicsEngine();
+    if (!physEngine) {
+      throw new Error("Unable to get physic engine");
+    } else {
+      engine.runRenderLoop((): void => {
+        scene.render();
+      });
+    }
+  }
 }
